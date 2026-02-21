a48ae928640fff55436fabf8fe32fe5769f26f14
M-Nowfal <nowfalmmuhammed@gmail.com>
2026-02-21 16:51:41 +0530

    next.config.ts updated.

--- next.config.ts
+++ next.config.ts
@@ -12,7 +12,22 @@ const nextConfig: NextConfig = {
   },
   typescript: {
     ignoreBuildErrors: true,
-  }
+  },
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your API
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          // Note: In strict production, replace "*" with your actual frontend domain "https://your-frontend.com"
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
